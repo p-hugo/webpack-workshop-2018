997ee7cdd1449861ef1943d125a43dc2f88d4e81
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-07-11 19:41:35 -0400

    + webpack html plugin and progress plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const webpack = require("webpack"); // default builtin plugins
+const htmlWebpackPlugin = require("html-webpack-plugin"); // external plugin
 module.exports = env => {
   return {
     mode: env.mode,
     output: {
       filename: "bundle.js"
-    }
+    },
+    plugins: [new htmlWebpackPlugin(), new webpack.ProgressPlugin()]
   };
 };
